e92733c49c933eff5826d5cfb2659e7c423a4325
Büşra Çetinkaya <80969758+busracetinkayaaa@users.noreply.github.com>
2023-07-12 14:00:31 +0300

    Giriş ekranı, butonlar ve yönlendirilen sahneler eklendi.

---
+++ Beezy/Assets/Scripts/BusraScript/SceneLoader.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadLevel(string Busra )
+    {
+        SceneManager.LoadScene("Busra");
+    }
+
+    public void LoadLevelSettings(string Settings)
+    {
+        SceneManager.LoadScene("Settings");
+    }
+    public void LoadLevelEnd(string GameOver)
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+}
